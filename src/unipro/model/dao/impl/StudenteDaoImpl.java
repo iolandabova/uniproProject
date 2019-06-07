ba73d461f805b910b1664c345fe9e7bcea38c818
Protom <Protom@NB3-AULA2-1P>
2019-06-07 12:24:42 +0200

    commit StudenteDaoImpl iole

--- src/unipro/model/dao/impl/StudenteDaoImpl.java
+++ src/unipro/model/dao/impl/StudenteDaoImpl.java
@@ -3,9 +3,11 @@ package unipro.model.dao.impl;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import login.model.Utente;
 import unipro.connection.DbConnection;
 import unipro.model.Studente;
 import unipro.model.dao.StudenteDAO;
@@ -21,13 +23,49 @@ public class StudenteDaoImpl implements StudenteDAO {
 
 	@Override
 	public void save(Studente s) {
-		// TODO Auto-generated method stub
-
+		
+		String query="insert into studente(matricola, nome, cognome, sesso, datanascita, indirizzo, citta, email, password) values (?,?,?,?,?,?,?,?,?)";
+		
+		try {
+			PreparedStatement ps = dbConn.getConn().prepareStatement(query);
+			ps.setString(1, s.getMatricola());
+			ps.setString(2, s.getNome());
+			ps.setString(3, s.getCognome());
+			ps.setString(4, s.getSesso());
+			Date d = s.getDataNascita();
+			java.sql.Date sd = new java.sql.Date(d.getTime());
+			ps.setDate(5, sd);
+			ps.setString(6, s.getIndirizzo());
+			ps.setString(7, s.getCitta());
+			ps.setString(8, s.getEmail());
+			ps.setString(9, s.getPassword());
+			
+			ps.executeUpdate();
+			
+			ps.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void deleteByMatricola(String matricola) {
-		// TODO Auto-generated method stub
+		
+		String query="delete from studente where matricola=?";
+	
+		try {
+			PreparedStatement ps = dbConn.getConn().prepareStatement(query);
+			ps.setString(1, matricola);
+			ps.executeUpdate();
+			
+			ps.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
@@ -71,14 +109,63 @@ public class StudenteDaoImpl implements StudenteDAO {
 
 	@Override
 	public void update(Studente s) {
-		// TODO Auto-generated method stub
+		
+		String query="update studente set nome=?, cognome=?, sesso=?, datanascita=?, indirizzo=?, citta=?, email=?, password=? where matricola=?";
+		
+		try {
+			PreparedStatement ps = dbConn.getConn().prepareStatement(query);
+			ps.setString(1, s.getNome());
+			ps.setString(2, s.getCognome());
+			ps.setString(3, s.getSesso());
+			Date d = s.getDataNascita();
+			java.sql.Date sd = new java.sql.Date(d.getTime());
+			ps.setDate(4, sd);
+			ps.setString(5, s.getIndirizzo());
+			ps.setString(6, s.getCitta());
+			ps.setString(7, s.getEmail());
+			ps.setString(8, s.getPassword());
+			ps.setString(9, s.getMatricola());
+			
+			ps.executeUpdate();
+			
+			ps.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public List<Studente> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<Studente> listaStudenti=new ArrayList<Studente>();
+		try {
+			String query="select * from utente";
+			PreparedStatement ps=dbConn.getConn().prepareStatement(query);
+			ResultSet rs=ps.executeQuery();
+			
+			while(rs.next()) {
+				Studente s=new Studente();
+				s.setMatricola(rs.getString("matricola"));
+				s.setNome(rs.getString("nome"));
+				s.setCognome(rs.getString("cognome"));
+				s.setSesso(rs.getString("sesso"));
+				s.setDataNascita(rs.getDate("datanascita"));
+				s.setIndirizzo(rs.getString("indirizzo"));
+				s.setCitta(rs.getString("citta"));
+				s.setEmail(rs.getString("email"));
+				s.setPassword(rs.getString("password"));
+				
+				listaStudenti.add(s);
+			}
+			ps.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return listaStudenti;
 	}
-
+	
 }
